141127c0095d711fff06be10cd65a563a61a943c
Oliver Kaufmann <okaufmann91@gmail.com>
2023-10-21 01:16:16 +0200

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,42 @@
-module.exports =  {
-  overrides: [
-    {
-      files: [
-        "./**/*.ts"
-      ],
-      parser: "@typescript-eslint/parser",
-      extends:  [
-        'plugin:@typescript-eslint/recommended',
-      ],
-      parserOptions:  {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
-      rules: {
-        "indent": ["error", 2],
-        "@typescript-eslint/no-explicit-any": "off",
-      },
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    {
-      files: [
-        "./**/*.tsx",
-      ],
-      parser: "@typescript-eslint/parser",
-      extends:  [
-        'plugin:@typescript-eslint/recommended',
-      ],
-      parserOptions:  {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        jsx: true,
-      },
-      rules: {
-        "indent": ["error", 2],
-        "@typescript-eslint/no-explicit-any": "off",
-      },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    {
-      files: [
-        "./*.js"
-      ],
-      extends: [
-        'eslint:recommended',
-      ],
-      env: {
-        node: true
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        semi: false,
+        printWidth: 120,
       },
-      parserOptions:  {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-      },
-      rules: {
-        "indent": ["error", 2],
-        "no-unused-vars": "off",
-      }
-    },
-  ]
-};
+    ],
+  },
+}

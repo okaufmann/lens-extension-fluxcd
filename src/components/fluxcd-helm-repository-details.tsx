98eb7d8049cf5cbc45316f24ab85d5e97431e119
Oliver Kaufmann <okaufmann91@gmail.com>
2023-10-20 23:38:12 +0200

    chore: remove not existing properties and unused imports

--- src/components/fluxcd-helm-repository-details.tsx
+++ src/components/fluxcd-helm-repository-details.tsx
@@ -1,9 +1,8 @@
 import { Renderer, Common } from "@k8slens/extensions";
 import React from "react";
 import { HelmRepository } from "../k8s/fluxcd/sources/helmrepository";
-import { getStatusClass, getStatusText } from "../utils";
 
-const { Component: { DrawerItem, Badge } } = Renderer
+const { Component: { DrawerItem } } = Renderer
 
 
 export class FluxCDHelmRepositoryDetails extends React.Component<Renderer.Component.KubeObjectDetailsProps<HelmRepository>> {
@@ -16,7 +15,6 @@ export class FluxCDHelmRepositoryDetails extends React.Component<Renderer.Compon
         <DrawerItem name="Interval">{object.spec.interval}</DrawerItem>
         <DrawerItem name="Timeout">{object.spec.timeout}</DrawerItem>
         <DrawerItem name="Suspended">{object.spec.suspend === true ? 'Yes' : 'No'}</DrawerItem>
-        <DrawerItem name="Reconcile Strategy">{object.spec.reconcileStrategy}</DrawerItem>
         <DrawerItem name="Url">
           <a href="#" onClick={e => { e.preventDefault(); Common.Util.openBrowser(object.spec.url) }}>
             {object.spec.url}

ab01424d782e4e907d56624c1ee11b0421981656
Oliver Kaufmann <okaufmann91@gmail.com>
2023-10-21 00:16:27 +0200

    feat: add details for image policies

--- renderer.tsx
+++ renderer.tsx
@@ -26,6 +26,7 @@ import { FluxCDGitRepositoryDetails } from './src/components/details/sources/git
 import { FluxCDHelmReleaseDetails } from './src/components/details/helm/helm-release-details'
 import { FluxCDReceiverDetails } from './src/components/notification/receiver-details'
 import { FluxCDImageRepositoryDetails } from './src/components/details/imageautomation/image-repository-details'
+import { FluxCDImagePolicyDetails } from './src/components/details/imageautomation/image-policy-details'
 
 import { Kustomization } from './src/k8s/fluxcd/kustomization'
 import { Receiver } from "./src/k8s/fluxcd/notifications/receiver";
@@ -69,6 +70,14 @@ export default class FluxCDExtension extends Renderer.LensExtension {
       priority: 10,
       components: {
         Details: (props: Renderer.Component.KubeObjectDetailsProps<Receiver>) => <FluxCDImageRepositoryDetails {...props} />
+      }
+    },
+    {
+      kind: "ImagePolicy",
+      apiVersions: ["image.toolkit.fluxcd.io/v1beta1", "image.toolkit.fluxcd.io/v1beta2"],
+      priority: 10,
+      components: {
+        Details: (props: Renderer.Component.KubeObjectDetailsProps<Receiver>) => <FluxCDImagePolicyDetails {...props} />
       }
     },
     {
---
+++ src/components/details/imageautomation/image-policy-details.tsx
@@ -0,0 +1,63 @@
+import { Renderer } from "@k8slens/extensions";
+import React from "react";
+import { ImagePolicy } from "../../../k8s/fluxcd/image-automation/imagepolicy";
+import { lowerAndPluralize } from "../../../utils";
+import { crdStore } from "../../../k8s/core/crd";
+
+const { Component: { DrawerItem } } = Renderer
+
+interface ImagePolicyDetailsState {
+  crds: Renderer.K8sApi.CustomResourceDefinition[]
+}
+
+export class FluxCDImagePolicyDetails extends React.Component<Renderer.Component.KubeObjectDetailsProps<ImagePolicy>, ImagePolicyDetailsState> {
+  public readonly state: Readonly<ImagePolicyDetailsState> = {
+    crds: [],
+  }
+
+  getCrd(kind: string): Renderer.K8sApi.CustomResourceDefinition | null {
+    const { crds } = this.state
+
+    if (!kind) {
+      return null
+    }
+
+    if (!crds) {
+      return null;
+    }
+
+    return crds.find(crd => crd.spec.names.kind === kind) ?? null
+  }
+
+  sourceUrl(resource: ImagePolicy): string {
+    const name = resource.spec.imageRepositoryRef.name
+    const ns = resource.spec.imageRepositoryRef.namespace ?? resource.metadata.namespace
+    const kind = lowerAndPluralize( 'ImageRepository')
+    const crd = this.getCrd('ImageRepository')
+    const apiVersion = crd?.spec.versions?.find((v: any) => v.storage === true)?.name
+    const group = crd?.spec.group
+
+    if(!apiVersion || !group) return ''
+
+    return `/apis/${group}/${apiVersion}/namespaces/${ns}/${kind}/${name}`
+  }
+
+  async componentDidMount() {
+    crdStore.loadAll().then(l => this.setState({ crds: l as Renderer.K8sApi.CustomResourceDefinition[] }));
+  }
+
+  render() {
+    const { object } = this.props
+
+    return (
+      <div>
+        <DrawerItem name="Source">
+          <a href="#" onClick={e => { e.preventDefault(); Renderer.Navigation.showDetails(this.sourceUrl(object), true) }}>
+            ImageRepository:{object.spec.imageRepositoryRef.name}
+          </a>
+        </DrawerItem>
+      </div>
+    )
+  }
+}
+
